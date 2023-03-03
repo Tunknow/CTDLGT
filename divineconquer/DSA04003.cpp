62965d59052ec0d4b7120f3523450b9797281967
Tunknow <tung75csp@gmail.com>
2023-03-03 16:30:28 +0700

    dem day

--- divineconquer/DSA04003.cpp
+++ divineconquer/DSA04003.cpp
@@ -4,24 +4,27 @@ using namespace std;
 
 int mod=123456789;
 
-long long Count(long long n)
+long long power(long long x, long long y)
 {
-	if(n==1) return 1;
-	
-	long long cnt=0;
-	for(int i=1; i<n; i++) {
-		cnt+=Count(n-i)%mod;
-	}
-	return (cnt+1)%mod;
+    long long temp;
+    if (y == 0)
+        return 1;
+    if(y==1) return x;
+    temp = power(x, y / 2);
+    if (y % 2 == 0)
+        return (temp * temp)%mod;
+    else
+        return (((temp * temp)%mod)*x)%mod;
 }
 
 int main()
 {
 	int t;
 	cin >> t;
-	while(t--) {
+	while(t--)
+	{
 		long long n;
 		cin >> n;
-		cout << Count(n) << endl;
+		cout <<	power(2, n-1)%mod << endl;
 	}
 }
