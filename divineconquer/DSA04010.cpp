31a60097171dc7b411151a8cd1a947c5fe937d2e
Tunknow <tung75csp@gmail.com>
2023-03-03 16:58:08 +0700

    day con lien tiep co tong lon nhat

---
+++ divineconquer/DSA04010.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define inf_min -10000000
+
+using namespace std;
+
+int a[100];
+int n;
+
+int maxCross(int l, int m, int r)
+{
+    int sum=0, lsum=inf_min, rsum=inf_min;
+    for(int i=m; i>=l; i--) {
+        sum+=a[i];
+        if(sum>lsum) lsum=sum;
+    }
+    sum=0;
+    for(int i=m+1; i<=r; i++) {
+        sum+=a[i];
+        if(sum>rsum) rsum=sum;
+    }
+    return lsum+rsum;
+}
+
+int maxSub(int l, int r)
+{
+    if(l==r) return a[l];
+    int m=(l+r)/2;
+    return max({maxSub(l,m), maxSub(m+1, r), maxCross(l, m, r)});
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        for(int i=0; i<n; i++) cin >> a[i];
+        cout << maxSub(0, n-1) << endl;
+    }
+}
