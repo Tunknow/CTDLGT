f1df8b151f8a4df651db0b1346d20bd0c22acbe3
Tunknow <tung75csp@gmail.com>
2023-03-11 16:09:44 +0700

    truc toa do

---
+++ Greedy/DSA03025.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+
+bool comp(pair<int, int> a, pair<int, int> b)
+{
+    return a.second < b.second;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        
+        vector<pair<int,int>> w(n);
+        for(int i=0; i<n; i++) cin >> w[i].first >> w[i].second;
+    
+        sort(w.begin(), w.end(),comp);
+        int cnt=1;
+        int i=0;
+        for(int j=1; j<n; j++) {
+            if(w[j].first >=w[i].second) {
+                i=j;
+                cnt++;
+            }
+        }
+        cout << cnt << endl;
+    }
+}
